f2f6daf27341a1a701c677aa0ab57dfc78dcb189
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-03 20:05:01 +0530

    Add initial Makefile and Event model structure

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+start:
+@./bin/main
 build:
  @go build -o ./bin ./cmd/api/main.go
---
+++ cmd/api/main.go
@@ -0,0 +1,13 @@
+package models
+ type Event struct{
+	Id string
+	Name string
+	Location string 
+	Date string
+	createdAt string
+	UpdatedAt string
+
+ }
+  type EventRepository struct {
+
+  }
