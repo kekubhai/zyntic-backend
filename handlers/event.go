26bc5822dbd8e2621f3c30e44104238d8b5a5f27
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-04 08:09:04 +0530

    Implement GetMany method in EventHandler and EventRepository, and define Event model structure

--- handlers/event.go
+++ handlers/event.go
@@ -1,19 +1,37 @@
 package handlers
 
 import (
+	"context"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	models "github.com/kekubhai/zyntic/cmd/api"
 )
+
 type EventHandler struct {
 	repository models.EventRepository
 }
-func (h *EventHandler) GetMany(ctx *fiber.Ctx)error{
-	context,cancel:=
+
+func (h *EventHandler) GetMany(ctx *fiber.Ctx) error {
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+	events, err := h.repository.GetMany(context)
+if err !=nil {
+	return ctx.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+		"status":"fall",
+		"message":err.Error(),
+	})
 }
-func (h *EventHandler) GetOne(ctx *fiber.Ctx)error{
+return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+"status":"fail",
+"message":"",
+"data":events,
+})
+}
+func (h *EventHandler) GetOne(ctx *fiber.Ctx) error {
 	return nil
 }
-func (h *EventHandler) CreateOne(ctx *fiber.Ctx)error{
+func (h *EventHandler) CreateOne(ctx *fiber.Ctx) error {
 	return nil
 }
 
--- repositories/event.go
+++ repositories/event.go
@@ -2,26 +2,36 @@ package repositories
 
 import (
 	"context"
+	"time"
 
 	models "github.com/kekubhai/zyntic/cmd/api"
 )
+
 type EventRepository struct {
 	db any
 }
-func (r *EventRepository) GetMany(ctx context.Context)([]*models.Event,error){
+
+func (r *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) {
+	events := []*models.Event{}
+	events = append(events, &models.Event{
+		Id:        "j48489893838334484",
+		Name:      "Anirban",
+		Location:  "Kolkata",
+		Date:      time.Now(),
+		createdAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+}
+func (r *EventRepository) GetOne(ctx context.Context, eventId string) (*models.Event, error) {
 	return nil, nil
 }
-func (r *EventRepository) GetOne(ctx context.Context,eventId string)(*models.Event,error){
-	return nil, nil
-}
-func (r *EventRepository) CreateOne(ctx context.Context,event models.Event)(*models.Event,error){
+func (r *EventRepository) CreateOne(ctx context.Context, event models.Event) (*models.Event, error) {
 	return nil, nil
 }
 
-
-func NewEventRepositories(db any) models.EventRepository{
+func NewEventRepositories(db any) models.EventRepository {
 	return &EventRepository{
-		db:db,
+		db: db,
 	}
 
 }
