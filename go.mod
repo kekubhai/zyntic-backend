2399abeb1ecb6db6ff75b41c770eb17af8b2eb22
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-08 11:52:34 +0530

    Refactor event model and repository to use GORM; add methods for updating and deleting events

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/joho/godotenv v1.5.1
+	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
 
@@ -31,5 +32,4 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gorm.io/driver/postgres v1.5.11 // indirect
 )
--- repositories/event.go
+++ repositories/event.go
@@ -2,38 +2,74 @@ package repositories
 
 import (
 	"context"
-	"time"
 
 	"github.com/kekubhai/zyntic/models"
+	"gorm.io/gorm"
 )
 
 type EventRepository struct {
-	db any
+	db *gorm.DB
 }
-
 
 func (r *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) {
 	events := []*models.Event{}
-	events = append(events, &models.Event{
-		ID:        "j48489893838334484",
-		Name:      "Anirban",
-		Location:  "Kolkata",
-		Date:      time.Now(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	})
+
+	res := r.db.Model(&models.Event{}).Order("updated_at desc").Find(&events)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
 	return events, nil
 }
-func (r *EventRepository) GetOne(ctx context.Context, eventId string) (*models.Event, error) {
-	return nil, nil
-}
-func (r *EventRepository) CreateOne(ctx context.Context, event models.Event) (*models.Event, error) {
-	return nil, nil
+
+func (r *EventRepository) GetOne(ctx context.Context, eventId uint) (*models.Event, error) {
+	event := &models.Event{}
+
+	res := r.db.Model(event).Where("id = ?", eventId).First(event)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return event, nil
 }
 
-func NewEventRepositories(db any) models.EventRepository {
+func (r *EventRepository) CreateOne(ctx context.Context, event *models.Event) (*models.Event, error) {
+	res := r.db.Model(event).Create(event)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return event, nil
+}
+
+func (r *EventRepository) UpdateOne(ctx context.Context, eventId uint, updateData map[string]interface{}) (*models.Event, error) {
+	event := &models.Event{}
+
+	updateRes := r.db.Model(event).Where("id = ?", eventId).Updates(updateData)
+
+	if updateRes.Error != nil {
+		return nil, updateRes.Error
+	}
+
+	getRes := r.db.Model(event).Where("id = ?", eventId).First(event)
+
+	if getRes.Error != nil {
+		return nil, getRes.Error
+	}
+
+	return event, nil
+}
+
+func (r *EventRepository) DeleteOne(ctx context.Context, eventId uint) error {
+	res := r.db.Delete(&models.Event{}, eventId)
+	return res.Error
+}
+
+func NewEventRepository(db *gorm.DB) models.EventRepository {
 	return &EventRepository{
 		db: db,
 	}
-
 }
